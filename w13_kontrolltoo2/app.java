cfdb5ccf7fa278bc9a6a86fb571066b3d1bef8c2
Taavi Luur <55079697+tluur@users.noreply.github.com>
2020-05-25 08:14:53 +0300

    Add files via upload

--- w13_kontrolltoo2/app.java
+++ w13_kontrolltoo2/app.java
@@ -7,37 +7,45 @@ public class app {
     public static void main(String[] args) {
 
         Scanner sisse = new Scanner(System.in);
-       
+
         System.out.print("Sisesta pendli pikkus :");
         int Ppikkus = sisse.nextInt();
 
         System.out.print("Sisesta hammasrataste arv :");
         int hamArv = sisse.nextInt();
 
-        pendel yks1 = new pendel(Ppikkus);        
-        int val = (int)yks1.getVonkeperiood();
+        pendel yks1 = new pendel(Ppikkus);
+        int val = (int) yks1.getVonkeperiood();
 
         hammasr kaks2 = new hammasr(hamArv, yks1.getVonkeperiood());
-        int ham = (int)kaks2.getHammPöörded();
+        int ham = (int) kaks2.getHammPöörded();
 
         System.out.println();
-        System.out.println( hamArv + " hammasratast alustavad pöörlemisega. ");
+        System.out.println(hamArv + " hammasratast alustavad pöörlemisega. ");
         System.out.println();
         System.out.println("Pendli võnkeperiood on " + yks1.getVonkeperiood() + " s.");
         System.out.println();
 
-        
         Timer t = new Timer();
         t.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                int n = 0;
-                n = n + 1;
-                System.out.println("Tik");
-            }
-        }, 0, val*1000);
+            public int e = 0;
 
-       
+            public void run() {
+
+                System.out.println("Pendel tegi Tik ning  hammasratast on teinud " + ++e * ham + " pööret.");
+
+            }
+        }, 0, val * 1000);
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        t.cancel();
+
+        System.out.println();
+        System.out.println("Ajaline piirang.");
+        System.out.println("Seiskumine.");
 
     }
 
